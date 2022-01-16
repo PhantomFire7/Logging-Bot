fe6f4c20f320c44aef720f09a27c9e131e7a35c0
BreadKitten <90331269+BreadCatto@users.noreply.github.com>
2022-01-16 17:41:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -31,4 +31,10 @@ async def on_message_edit(before, after):
 
     await logs.send(embed=embed)
 
+@client.event
+async def on_channel_create(channel):
+    embed = discord.Embed(title="A new channel created", description=f"**Name:** {channel.name}\n**Category:** {channel.category.name}\n**Channel id:** {channel.id}")
+    embed.set_footer(text=f"id: {channel.id}")
+
+
 client.run(TOKEN)
